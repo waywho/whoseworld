bbcb182c9ffa768143ec0ad369e912df90726361
WeiHsi Hu <walzerfan@yahoo.com>
2022-04-03 22:43:09 +0200

    Add accounts for multi-tenant

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
+    './app/assets/stylesheets/**/*.scss',
+    './app/javascript/**/*.js',
+    './config/initializers/simple_form_tailwind.rb'
   ]
 }
