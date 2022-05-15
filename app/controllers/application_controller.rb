896c6ed7ac46cc6da59156771965d1d82080e530
WeiHsi Hu <walzerfan@yahoo.com>
2022-05-15 13:41:16 +0200

    redirect to coming soon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  include DevRedirect
+
+  private
+
+  def dev_redirect
+    if Rails.env.production?
+      redirect_to root_path
+    end
+  end
 end
