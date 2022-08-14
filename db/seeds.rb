a35a4b881ffd2b8df521992e091fe2696ea90b7b
WeiHsi Hu <walzerfan@yahoo.com>
2022-08-14 13:02:18 +0200

    correct seed and user
    update again

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,32 +6,27 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Site.upsert_all([{
+[{
   name: 'Storychor',
   domain: 'storychor.com',
-  slug: 'storychor',
   orientation: 'top',
-  subtitle: nil,
   template_style: :multi_page
 }, {
   name: 'WeiHsi Hu',
   domain: 'weihsihu.com',
-  slug: 'weihsihu',
   orientation: 'left',
   subtitle: 'conductor, tenor',
   template_style: :one_page
 }, {
   name: 'Logical Thinking',
   domain: 'logical-thinking.co.uk',
-  slug: 'logical_thinking',
   orientation: 'top',
-  sutbitle: nil,
   template_style: :multi_page
 }, {
   name: 'Berlin UnRehearsed Musicals',
   domain: 'berlinunrehearsedmusicals.com',
-  slug: 'berlin_unrehearsed_musicals',
   orientation: 'top',
-  sutbitle: nil,
   template_style: :multi_page
-}])
+}].each do |site|
+  Site.upsert(site)
+end
