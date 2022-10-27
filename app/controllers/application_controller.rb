c03d4aa4c2c15a60bc2077c0176e90fa0a246a55
WeiHsi Hu <walzerfan@yahoo.com>
2022-10-27 21:09:11 +0200

    Check domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,22 +10,21 @@ class ApplicationController < ActionController::Base
     Current.tenant = if Rails.env.development?
       domain = request.domain.split(".")[0]
       domain = "weihsihu" if domain == "localhost"
-      site = Site.where("domain LIKE ?", "%#{domain}%").first
+      Site.where("domain LIKE ?", "%#{domain}%").first
     else
-      unless site = Site.find_by(domain: current_domain)
-        domain_alias = DomainAlias.find_by(domain: current_domain)
-        site = domain_alias.site if domain_alias
+      if site = Site.find_by(domain: current_domain)
+        site
+      elsif domain_alias = DomainAlias.find_by(domain: current_domain)
+        domain_alias.site
       end
-      site
     end
 
     Current.style = Current.tenant&.template_style || :multi_page
   end
 
   def current_domain
-    if request.subdomain == "www"
-      request.domain
-    elsif request.domain == "co.uk"
+    if request.domain == "co.uk"
+      Rails.logger.info "Request Domain(2): #{request.domain(2)}"
       request.domain(2)
     else
       request.domain
