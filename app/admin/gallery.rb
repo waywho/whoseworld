30a22706b9c1cb35e07d1e2eaa6974bdde77d1a8
WeiHsi Hu <walzerfan@yahoo.com>
2024-12-31 17:53:00 +0100

    Create gallery.rb

---
+++ app/admin/gallery.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+ActiveAdmin.register Gallery do
+  permit_params :title, :description, :page_id, :feature, :row_order
+  form partial: "form"
+
+  index do
+    column :page
+    column :row_order
+    column :title
+    column :description
+    column :feature
+    actions
+  end
+end
