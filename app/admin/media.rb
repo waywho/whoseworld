74efcb977b9cda1c8fbae6927fb7c4e66600a9ea
WeiHsi Hu <walzerfan@yahoo.com>
2024-12-31 17:39:51 +0100

    Switch admin/media to active_admin

---
+++ app/admin/media.rb
@@ -0,0 +1,24 @@
+ActiveAdmin.register Media do
+  permit_params :title, :source, :page_id, :site_id, :media_type, :row_order
+  config.sort_order = "row_order_asc"
+
+  Site.all.each do |site|
+    scope(site.slug) { |scope| scope.where(site:) }
+  end
+
+  index do
+    column :page
+    column :title
+    column :media_type
+    column :row_order
+  end
+
+  form do |f|
+    f.input :title
+    f.input :source
+    f.input :media_type, as: :select, collection: Media.types
+    f.input :row_order, label: "Order"
+    f.input :page
+    f.input :site
+  end
+end
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,9 +1,14 @@
 ActiveAdmin.register User do
   permit_params :email, :password, :password_confirmation
 
+  scope :all, default: true
+  scope("Admin") { |scope| scope.where(admin: true) }
+
+
   index do
     selectable_column
     id_column
+    column :admin
     column :email
     column :current_sign_in_at
     column :sign_in_count
@@ -19,6 +24,7 @@ ActiveAdmin.register User do
   form do |f|
     f.inputs do
       f.input :email
+      f.input :admin
       f.input :password
       f.input :password_confirmation
     end
