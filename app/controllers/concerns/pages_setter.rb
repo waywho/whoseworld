0732888d717a202f48725ab1b8659b95349af866
WeiHsi Hu <walzerfan@yahoo.com>
2025-01-04 17:14:50 +0100

    Update pages_setter.rb

--- app/controllers/concerns/pages_setter.rb
+++ app/controllers/concerns/pages_setter.rb
@@ -4,7 +4,7 @@ module PagesSetter
   private
 
   def set_pages(site)
-    if site.layout_style == :one_page
+    if site.one_page?
       site.pages.menu_pages.includes(:medias, :galleries).rank(:row_order)
     else
       site.pages.feature.rank(:row_order)
