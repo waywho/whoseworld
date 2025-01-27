c937a54a4fbc04179972294f5d0b678a827f537c
WeiHsi Hu <walzerfan@yahoo.com>
2025-01-27 23:32:44 +0100

    Add Signup Mailer

--- test/factories/burm/musicals.rb
+++ test/factories/burm/musicals.rb
@@ -2,10 +2,14 @@ FactoryBot.define do
   factory :burm_musical, class: "BURM::Musical" do
     title { Faker::Movie.title }
     start_at { "2024-12-30 15:54:48" }
-    end_at { "2024-12-30 15:54:48" }
+    end_at { nil }
     location { "MyString" }
     fee { 15.00 }
     signup_start_at { Time.current - 1.day }
+
+    after(:build) do |musical|
+      musical.end_at = musical.start_at + 6.hours
+    end
 
     trait :with_roles do
       after(:build) do |musical|
