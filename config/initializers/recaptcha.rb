4a25cc6236b92a40333e06b1ef2588ef227a4ee2
WeiHsi Hu <walzerfan@yahoo.com>
2025-03-01 09:25:19 +0100

    fix failing tests

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key  = Rails.application.credentials.recaptcha_3[:site_key]
-  config.secret_key = Rails.application.credentials.recaptcha_3[:site_secret]
+  config.site_key  = Rails.application.credentials.dig(:recaptcha_3, :site_key)
+  config.secret_key = Rails.application.credentials.dig(:recaptcha_3, :site_secret)
 end
