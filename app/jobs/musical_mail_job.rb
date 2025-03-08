15f9d2387969ea9e0a23ebce0f65a9f289a74642
WeiHsi Hu <walzerfan@yahoo.com>
2025-03-08 15:23:33 +0100

    Update musical_mail_job.rb

--- app/jobs/musical_mail_job.rb
+++ app/jobs/musical_mail_job.rb
@@ -3,6 +3,6 @@ class MusicalMailJob < ApplicationJob
 
   def perform(mail_method, musical, test: false)
     receivers = BURM::Person.where(email: "weihsi.hu@gmail.com") if test
-    MusicalMailerService.new("BURM::MuiscalMailer", mail_method, musical, receivers:).send_mail
+    MusicalMailerService.new("BURM::MuiscalMailer", mail_method, musical:, receivers:).send_mail
   end
 end
