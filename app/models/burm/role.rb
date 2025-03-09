c85149033d0a40feb8b0dced438565349ad1dde5
WeiHsi Hu <walzerfan@yahoo.com>
2025-03-10 00:10:33 +0100

    add type

--- app/models/burm/role.rb
+++ app/models/burm/role.rb
@@ -7,7 +7,7 @@ class BURM::Role < ApplicationRecord
   has_many :signups, class_name: "BURM::Signup", foreign_key: "burm_role_id", dependent: :nullify
 
   # Enums
-  enum :voice_type, %i[speaking bass baritone tenor alto mezzo_soprano soprano], allow_nil: true, validate: true
+  enum :voice_type, %i[speaking bass baritone tenor alto mezzo_soprano soprano satb], allow_nil: true, validate: true
   enum :role_type, %i[ensemble featured supporting lead], validate: { allow_nil: true }, suffix: :role
   
   # Validations
